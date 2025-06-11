9080567d51e21e736146200ec44367581207d9fe
ReSech <resech@chesssorrel.us>
2025-06-11 14:58:51 +1000

    Stripping un-needed columns from CSVs

--- cubes/.cube_update.ps1
+++ cubes/.cube_update.ps1
@@ -7,14 +7,16 @@ $oneweekago = (Get-Date -AsUTC).AddDays(-7)
 foreach ($cube_code in $cubes) {
   if ( -not (Test-Path ".\$cube_code.csv")) {
       $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code"
-      Invoke-WebRequest $cubeurl -OutFile ".\$cube_code.csv"
+      $cubedata = Invoke-WebRequest $cubeurl | ConvertFrom-Csv
+      $cubedata | select name,Type,Color,"Color Category",maybeboard,tags | Export-Csv -Path ".\$cube_code.csv" –NoTypeInformation
     } else {
       $cubedateapi = "https://cubecobra.com/cube/api/date_updated/" + "$cube_code"
       $response = Invoke-RestMethod -URI "$cubedateapi"
       $lastupdate = ([datetime] '1970-01-01Z').ToUniversalTime().AddSeconds($response.date_updated/1000)
       if ($lastupdate -gt $oneweekago) {
           $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code"
-          Invoke-WebRequest $cubeurl -OutFile ".\$cube_code.csv"
+          $cubedata = Invoke-WebRequest $cubeurl | ConvertFrom-Csv
+          $cubedata | select name,Type,Color,"Color Category",maybeboard,tags | Export-Csv -Path ".\$cube_code.csv" –NoTypeInformation
       }
     }
 }
@@ -24,14 +26,16 @@ foreach ($cube_code in $cubes) {
 $cube_code = "j25-tight"
 if ( -not (Test-Path ".\$cube_code.csv")) {
   $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code" + '?primary=Tags&secondary=Unsorted&tertiary=Unsorted&quaternary=Mana%20Value&showother=false&filter=-tags%3AToken%20-t%3Atoken'
-  Invoke-WebRequest $cubeurl -OutFile ".\$cube_code.csv"
+  $cubedata = Invoke-WebRequest $cubeurl | ConvertFrom-Csv
+  $cubedata | select name,Type,Color,"Color Category",maybeboard,tags | Export-Csv -Path ".\$cube_code.csv" –NoTypeInformation
 } else {
   $cubedateapi = "https://cubecobra.com/cube/api/date_updated/" + "$cube_code"
   $response = Invoke-RestMethod -URI "$cubedateapi"
   $lastupdate = ([datetime] '1970-01-01Z').ToUniversalTime().AddSeconds($response.date_updated/1000)
   if ($lastupdate -gt $oneweekago) {
       $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code" + '?primary=Tags&secondary=Unsorted&tertiary=Unsorted&quaternary=Mana%20Value&showother=false&filter=-tags%3AToken%20-t%3Atoken'
-      Invoke-WebRequest $cubeurl -OutFile ".\$cube_code.csv"
+      $cubedata = Invoke-WebRequest $cubeurl | ConvertFrom-Csv
+      $cubedata | select name,Type,Color,"Color Category",maybeboard,tags | Export-Csv -Path ".\$cube_code.csv" –NoTypeInformation
   }
 }
 
@@ -40,31 +44,43 @@ if ( -not (Test-Path ".\$cube_code.csv")) {
 $cube_code = "kvatchstart"
 if ( -not (Test-Path ".\$cube_code.csv")) {
   $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code" + '?primary=Tags&secondary=Unsorted&tertiary=Unsorted&quaternary=Mana%20Value&showother=false&filter=-tags%3AToken%20-t%3Atoken'
-  Invoke-WebRequest $cubeurl -OutFile ".\$cube_code.csv"
+  $cubedata = Invoke-WebRequest $cubeurl | ConvertFrom-Csv
+  foreach ($card in $cubedata) {
+    if ($card.'Color Category' -eq "Lands" -and $card.tags -eq "z_Fixing Roster_z") {
+      if ($card.Color -eq "") {
+        $scryfallapi = "https://api.scryfall.com/cards/named?exact=" + [System.Web.HttpUtility]::UrlDecode($card.name)
+        $scryfalloutput = Invoke-RestMethod -URI "$scryfallapi"
+        $temp_color = "";
+        foreach ($color in $scryfalloutput.produced_mana) {
+          $temp_color += $color
+        }
+        $card.Color = $temp_color
+      }
+    }
+  }
+  $selectedcolumns = $cubedata | select name,Type,Color,"Color Category",maybeboard,tags 
+  $selectedcolumns | Export-Csv -Path ".\$cube_code.csv" –NoTypeInformation
 } else {
   $cubedateapi = "https://cubecobra.com/cube/api/date_updated/" + "$cube_code"
   $response = Invoke-RestMethod -URI "$cubedateapi"
   $lastupdate = ([datetime] '1970-01-01Z').ToUniversalTime().AddSeconds($response.date_updated/1000)
   if ($lastupdate -gt $oneweekago) {
-      $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code" + '?primary=Tags&secondary=Unsorted&tertiary=Unsorted&quaternary=Mana%20Value&showother=false&filter=-tags%3AToken%20-t%3Atoken'
-      Invoke-WebRequest $cubeurl -OutFile ".\$cube_code.csv"
-  }
-}
-
-$kvatchstart_cube = Get-Content ".\$cube_code.csv" | ConvertFrom-Csv
-
-foreach ($card in $kvatchstart_cube) {
-  if ($card.'Color Category' -eq "Lands" -and $card.tags -eq "z_Fixing Roster_z") {
-    if ($card.Color -eq "") {
-      $scryfallapi = "https://api.scryfall.com/cards/named?exact=" + [System.Web.HttpUtility]::UrlDecode($card.name)
-      $scryfalloutput = Invoke-RestMethod -URI "$scryfallapi"
-      $temp_color = "";
-      foreach ($color in $scryfalloutput.produced_mana) {
-        $temp_color += $color
+    $cubeurl = "https://cubecobra.com/cube/download/csv/" + "$cube_code" + '?primary=Tags&secondary=Unsorted&tertiary=Unsorted&quaternary=Mana%20Value&showother=false&filter=-tags%3AToken%20-t%3Atoken'
+    $cubedata = Invoke-WebRequest $cubeurl | ConvertFrom-Csv
+    foreach ($card in $cubedata) {
+      if ($card.'Color Category' -eq "Lands" -and $card.tags -eq "z_Fixing Roster_z") {
+        if ($card.Color -eq "") {
+          $scryfallapi = "https://api.scryfall.com/cards/named?exact=" + [System.Web.HttpUtility]::UrlDecode($card.name)
+          $scryfalloutput = Invoke-RestMethod -URI "$scryfallapi"
+          $temp_color = "";
+          foreach ($color in $scryfalloutput.produced_mana) {
+            $temp_color += $color
+          }
+          $card.Color = $temp_color
+        }
       }
-      $card.Color = $temp_color
     }
+    $selectedcolumns = $cubedata | select name,Type,Color,"Color Category",maybeboard,tags 
+    $selectedcolumns | Export-Csv -Path ".\$cube_code.csv" –NoTypeInformation
   }
 }
-
-$kvatchstart_cube | Export-Csv -Path ".\$cube_code.csv"
