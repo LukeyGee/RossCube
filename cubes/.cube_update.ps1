56774ac8454ea150f6108a495d06209730aaece5
ReSech <resech@chesssorrel.us>
2025-06-30 12:37:45 +1000

    Enhance cube CSV download reliability with retry logic

--- cubes/.cube_update.ps1
+++ cubes/.cube_update.ps1
@@ -23,7 +23,7 @@ $cubes = @("trs", "krstart1", "infjumpstartcube", "ajsc", "jumpstartdecks", "jum
 
 foreach ($cube_code in $cubes) {
   $cubeUrl = "https://cubecobra.com/cube/download/csv/" + "$cube_code"
-  $csvText = [Text.Encoding]::UTF8.GetString((Invoke-WebRequest $cubeUrl).RawContentStream.ToArray())
+  $csvText = [Text.Encoding]::UTF8.GetString((Invoke-WebRequest -MaximumRetryCount 5 -RetryIntervalSec 10 $cubeUrl).RawContentStream.ToArray())
   $cubeData = $csvText | ConvertFrom-Csv | Select-Object *, @{Name='manacost'; Expression={""}}
   foreach ($card in $cubeData) {
     if ($scryHash.ContainsKey($card.name)) {
@@ -65,7 +65,7 @@ foreach ($cube_code in $cubes) {
 $cubes = @("j25-tight", "j22-tight", "jmp2020tight")
 foreach ($cube_code in $cubes) {
   $cubeUrl = "https://cubecobra.com/cube/download/csv/" + "$cube_code" + '?primary=Tags&secondary=Unsorted&tertiary=Unsorted&quaternary=Mana%20Value&showother=false&filter=-tags%3AToken%20-t%3Atoken'
-  $csvText = [Text.Encoding]::UTF8.GetString((Invoke-WebRequest $cubeUrl).RawContentStream.ToArray())
+  $csvText = [Text.Encoding]::UTF8.GetString((Invoke-WebRequest -MaximumRetryCount 5 -RetryIntervalSec 10 $cubeUrl).RawContentStream.ToArray())
   $cubeData = $csvText | ConvertFrom-Csv | Select-Object *, @{Name='manacost'; Expression={""}}
   foreach ($card in $cubeData) {
     if ($scryHash.ContainsKey($card.name)) {
@@ -106,7 +106,7 @@ foreach ($cube_code in $cubes) {
 # We need to make sure some of the lands have colors associated to them
 $cube_code = "kvatchstart"
 $cubeUrl = "https://cubecobra.com/cube/download/csv/" + "$cube_code"
-$csvText = [Text.Encoding]::UTF8.GetString((Invoke-WebRequest $cubeUrl).RawContentStream.ToArray())
+$csvText = [Text.Encoding]::UTF8.GetString((Invoke-WebRequest -MaximumRetryCount 5 -RetryIntervalSec 10 $cubeUrl).RawContentStream.ToArray())
 $cubeData = $csvText | ConvertFrom-Csv | Select-Object *, @{Name='manacost'; Expression={""}}
 foreach ($card in $cubeData) {
   if ($scryHash.ContainsKey($card.name)) {
